862b34d407e3a47c7332b39d7732f3c404925390
misbahasim <39546951+misbahasim@users.noreply.github.com>
2021-07-19 23:16:42 +0500

    jquery updated
    form validated and script moved to another file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    $(window).on("load", function () {
+        $(".loader-container").fadeOut("fast");
+    });
+
     $('#sideIcon').click(function () {
         $(this).toggleClass("fa-bars fa-times");
         $("nav").toggleClass("nav-toggle");
@@ -41,7 +45,90 @@ $(document).ready(function () {
         }
     });
 
-    $(window).on("load", function () {
-        $(".loader-container").fadeOut();
+    $("#name_err_msg").hide();
+    $("#email_err_msg").hide();
+    $("#message_err_msg").hide();
+
+    var error_name = false;
+    var error_email = false;
+    var error_message = false
+
+    $("#name").blur(function () {
+        checkFname();
     });
+    $("#mail").blur(function () {
+        checkEmail();
+    });
+    $("#message").blur(function () {
+        checkMessage();
+    });
+
+    function checkFname() {
+        var pattern = /^[a-zA-Z ]*$/;
+        var fname = $("#name").val();
+        if (pattern.test(fname) && fname !== '') {
+            $("#name_err_msg").hide();
+            $("#name").css("border", "2px solid #34F458");
+        }
+        else {
+            $("#name_err_msg").html("Should contain only letters");
+            $("#name_err_msg").show();
+            $("#name").css("border", "2px solid #F90A0A");
+            error_name = true;
+        }
+    }
+    function checkEmail() {
+        var pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        var email = $("#mail").val();
+        if (pattern.test(email) && email !== '') {
+            $("#email_err_msg").hide();
+            $("#mail").css("border", "2px solid #34F458");
+        }
+        else {
+            $("#email_err_msg").html("Invalid email");
+            $("#email_err_msg").show();
+            $("#mail").css("border", "2px solid #F90A0A");
+            error_email = true;
+        }
+    }
+    function checkMessage() {
+        var pattern = /^[a-zA-Z ]*$/;
+        var message = $("#message").val();
+        var msg_lenght = $("#message").val().length;
+
+        // if(msg_lenght<3 || msg_lenght >20){
+        //     should be between 5-20 characters
+        // }
+        if (!pattern.test(message) || msg_lenght < 20) {
+            $("#message_err_msg").html("At least 20 letters");
+            $("#message_err_msg").show();
+            $("#message").css("border", "2px solid #F90A0A");
+            error_message = true;
+        }
+        else {
+            $("#message_err_msg").hide();
+            $("#message").css("border", "2px solid #34F458");
+        }
+    }
+
+    $("#registration_form").submit(function () {
+
+        error_name = false;
+        error_email = false;
+        error_message = false;
+
+        checkFname();
+        checkEmail();
+        checkMessage();
+
+        if (error_name === false && error_email === false && error_message === false) {
+            $("#submission").html("Thank you for contacting us");
+            return true;
+        } else {
+            $("#submission").html("Fill the form correctly");
+            $("#submission").css({ "background": "#ff3300", "color": "#fff", "font-size": "2rem", "text-align": "center" });
+            return false;
+        }
+    });
+
 });
